cb5ac20f6f52b3bf45d4492c4bfdf3524c721952
Junho Oh <Junho Oh>
2017-02-09 00:00:50 +0900

    spring first commit

---
+++ src/main/java/com/springboot/exercise/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.springboot.exercise;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootExerciseApplication.class);
+	}
+
+}
